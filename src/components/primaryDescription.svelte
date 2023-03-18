7c2ef51e6cd21cf80f44e2b83c4fc5b347de35d3
Your Karim DAHOUMANE <krmdahoumane@gmail.com>
2023-03-18 17:48:13 +0100

    feat(car-page): Get and display data from a specific car

---
+++ src/components/primaryDescription.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	interface PrimaryDescriptionProps {
+		model: string;
+		year: number;
+		price: number;
+		dealerName: string;
+	}
+
+	export let props: PrimaryDescriptionProps;
+</script>
+
+<div>
+	<h1>Model: {props.model}</h1>
+	<p>Year: {props.year}</p>
+	<p>Price: {props.price}$</p>
+	<p>Dealer: {props.dealerName}</p>
+
+	<button>Book online</button>
+</div>
