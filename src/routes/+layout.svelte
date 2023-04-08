f707d124fe9fb98d1da9f70247870348c587d89c
3kezoh <3kezoh@gmail.com>
2023-04-08 10:19:53 +0200

    feat: add home page title and description

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script>
 	import "../app.css";
+	import { page } from "$app/stores";
 </script>
+
+<svelte:head>
+	<title>{$page.data.title}</title>
+	<meta name="description" content={$page.data.description} />
+</svelte:head>
 
 <header>
 	<nav
