a5b7b471d2a1283e5765961176e3891bdd488ad6
Your Karim DAHOUMANE <krmdahoumane@gmail.com>
2023-04-08 16:22:50 +0200

    feat(carousel): Replace with new carousel

---
+++ src/components/Carousel.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import type { Images } from "../types/car.type";
+
+	export let images: Images[];
+</script>
+
+<div class="wrapper">
+	<div class="gallery scroll-x scroll-mandatory snap-x snap-mandatory">
+		{#each images as image}
+			<img class="snap-center snap-always" src={image.path} alt="" />
+		{/each}
+	</div>
+</div>
+
+<style>
+	.wrapper {
+		height: 250px;
+		width: 400px;
+		margin-bottom: 20px;
+	}
+
+	.gallery {
+		display: flex;
+		flex-direction: row;
+		overflow: scroll;
+		overflow-y: hidden;
+	}
+
+	img {
+		max-height: 100%;
+		object-fit: cover;
+		margin-right: 10px;
+	}
+</style>
