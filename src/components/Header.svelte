613d6716ed33b9753f9142c77bc2fcff8c396047
Prado <mahery.rsh@gmail.com>
2023-04-11 23:37:33 +0200

    chore: review base layout

---
+++ src/components/Header.svelte
@@ -0,0 +1,78 @@
+<header>
+	<nav
+		class="top-0 z-10 fixed w-screen bg-nav text-chinese-silver tracking-[0.25px] font-bold grid grid-flow-col items-center h-16 lg:h-24 px-4 lg:px-14"
+	>
+		<div class="flex gap-4 lg:gap-10 items-center">
+			<a href="/" aria-label="Go to the home page">
+				<svg viewBox="0 0 30 30" class="h-8 lg:h-12">
+					<path
+						d="M15 0C6.716 0 0 6.716 0 15s6.716 15 15 15 15-6.716 15-15S23.284 0 15 0zm14.025 15A14.03 14.03 0 0 1 15 29.025 14.03 14.03 0 0 1 .975 15 14.03 14.03 0 0 1 15 .975 14.03 14.03 0 0 1 29.025 15zM22.27 8.636c.204.219.497.571.662.786l3.038-1.928-.575-.713-1.923 1.272-.132.113.098-.143.85-1.695-.601-.601-1.696.85-.143.098.114-.132 1.273-1.923-.714-.576-1.928 3.034.772.652 1.815-.942.116-.085-.085.116zM15.377 4.79l.83-1.859.053-.165-.015.172.088 2.484.888.117-.135-3.679-1.24-.096-.818 2.029-.029.151-.029-.151-.817-2.029-1.24.096-.135 3.679.889-.117.087-2.484-.015-.172.053.165.83 1.859zM8.665 7.722c.48-.505.751-1.089.272-1.646-.261-.303-.7-.369-1.063-.22l-.036.014.011-.032a.88.88 0 0 0-.302-.919.91.91 0 0 0-.671-.187c-.451.046-.798.354-1.741 1.406l-.948 1.143L6.779 9.74l1.886-2.018zm-3.382-.601c.522-.639 1.077-1.218 1.328-1.447.079-.073.166-.145.271-.173.17-.047.345.074.385.248s-.07.343-.193.48L5.786 7.6zm1.015.963l1.308-1.376c.128-.132.21-.213.298-.256.114-.055.238-.067.349.007s.16.2.132.325c-.032.149-.154.291-.254.399L6.824 8.582zM23.895 15H15V6.105c4.916 0 8.895 3.979 8.895 8.895zM15 15v8.895c-4.916 0-8.895-3.979-8.895-8.895z"
+						class="fill-white"
+					/>
+					<path
+						d="M15 6.105V15H6.105c0-4.916 3.979-8.895 8.895-8.895zM23.895 15c0 4.916-3.979 8.895-8.895 8.895V15z"
+						class="fill-honolulu-blue"
+					/>
+				</svg>
+			</a>
+			<ul class="hidden lg:flex lg:gap-10 drop-shadow-nav">
+				<li>
+					<a class="hover:text-white" href="/">Models</a>
+				</li>
+				<li>
+					<a class="hover:text-white" href="/">Build Your Own</a>
+				</li>
+				<li>
+					<a class="hover:text-white" href="/">Shopping</a>
+				</li>
+				<li>
+					<a class="hover:text-white" href="/">BMW Electric</a>
+				</li>
+				<li>
+					<a class="hover:text-white" href="/">Owners</a>
+				</li>
+			</ul>
+		</div>
+		<div class="flex text-xs lg:text-sm  gap-4 lg:gap-8 items-center justify-self-end">
+			<button class="group flex gap-1 hover:text-white">
+				<svg
+					class="fill-chinese-silver group-hover:fill-white h-[11px] w-[6px] relative top-[1px] lg:h-3 lg:w-[7px] lg:top-[2px] drop-shadow-nav"
+					viewBox="0 -0.384 3 5.756"
+				>
+					<path
+						d="M1.5 0C.671 0 0 .683 0 1.526c0 .493.312 1.158.572 1.69L1.5 5l.929-1.784C2.688 2.684 3 2.019 3 1.526 3 .683 2.329 0 1.5 0zm0 2.339c-.434 0-.786-.36-.786-.804S1.066.731 1.5.731s.786.36.786.804-.352.804-.786.804z"
+					/>
+				</svg>
+				<span class="drop-shadow-nav">Choose your local BMW Center</span>
+			</button>
+			<button class="hidden group lg:flex gap-1 hover:text-white">
+				<svg
+					class="fill-chinese-silver group-hover:fill-white h-[11px] relative top-[1px] lg:h-3 lg:top-[2px] drop-shadow-nav"
+					viewBox="0 0 12 12"
+				>
+					<path
+						d="M3.643 7.5c.69.473 1.494.75 2.357.75.863 0 1.667-.277 2.357-.75H9a3 3 0 0 1 3 3V12H0v-1.5a3 3 0 0 1 3-3zM6 0c1.45 0 2.625 1.343 2.625 3S7.45 6 6 6 3.375 4.657 3.375 3 4.55 0 6 0z"
+					/>
+				</svg>
+				<span class="drop-shadow-nav">My BMW</span>
+			</button>
+			<button
+				class="group hidden lg:flex justify-center items-center rounded-full lg:w-9 lg:h-9 bg-raisin-black/80 hover:bg-white"
+			>
+				<svg
+					class="fill-chinese-silver lg:h-3 group-hover:fill-raisin-black"
+					viewBox="0 0 1024 1024"
+				>
+					<path
+						d="M880.768 1018.496l-188.8-190.336c-70.144 46.4-154.24 73.536-244.992 73.536C199.872 901.696.064 699.712.064 450.56.064 201.92 199.872-.064 447.104-.064c246.592 0 447.04 201.92 447.04 450.624 0 87.04-24.384 168-66.368 236.672l190.528 192.064c13.632 13.44 2.304 46.848-25.088 74.496l-38.592 38.72c-27.456 27.776-60.672 39.232-73.856 25.984zM745.6 450.624c0-166.272-133.632-300.928-298.688-300.928-164.864.064-298.816 134.656-298.816 300.864 0 166.144 133.952 301.312 298.944 301.312 164.928 0 298.56-135.104 298.56-301.248z"
+					/>
+				</svg>
+			</button>
+			<div class="lg:hidden flex flex-col justify-between h-[12px] w-[21px]">
+				<div class="bg-chinese-silver h-[2px]" />
+				<div class="bg-chinese-silver h-[2px]" />
+				<div class="bg-chinese-silver h-[2px]" />
+			</div>
+		</div>
+	</nav>
+</header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,15 +3,13 @@
 	import Designs from "../components/home/Designs.svelte";
 	import Ownerships from "../components/home/Ownerships.svelte";
 	import Cars from "../components/home/Cars.svelte";
-	import Footer from "../components/Footer.svelte";
 </script>
 
-<div class="snap-y snap-mandatory h-d-screen overflow-scroll">
+<div class="snap-y snap-mandatory">
 	<Cars />
 	<div class="snap-start snap-always">
 		<Designs />
 		<Series />
 		<Ownerships />
-		<Footer />
 	</div>
 </div>
